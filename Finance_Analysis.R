722c958451e7147488cc843f07a22980be141af8
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-07-14 10:05:14 +0700

    Add files via upload

---
+++ Finance_Analysis.R
@@ -0,0 +1,6 @@
+personal_finance <- read.csv("/Users/heidi/Documents/personal_finance.csv")
+head(personal_finance)
+
+
+library(dplyr)
+#dplyr -> for data manipulation, operation (%>%) or (|>), filter(), select(), mutate()
